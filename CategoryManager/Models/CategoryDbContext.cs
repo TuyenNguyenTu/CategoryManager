a3fde0825aec2afdffc1ab87ddba4611596155ca
TuyenNguyen <tuyen.nguyen@amela.vn>
2020-07-08 21:31:39 +0700

    Add project files.

---
+++ CategoryManager/Models/CategoryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CategoryManager.Models
+{
+    public class CategoryDbContext :DbContext
+    {
+        public CategoryDbContext(DbContextOptions<CategoryDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { set; get; }
+    }
+}
